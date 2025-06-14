77b1222dc81388c1fe88a5fcb782f1d813cc8c22
yustyy <yusuf.acmaci@hotmail.com>
2025-06-14 18:36:08 +0300

    change style on supervisorymembers

--- src/Pages/supervisory.js
+++ src/Pages/supervisory.js
@@ -3,6 +3,9 @@ import { supervisoryMembers2526 } from "../Data/supervisoryData";
 import BoardMember from "../Components/boardMember";
 
 const SupervisoryPage = () => {
+  const firstRowMembers = supervisoryMembers2526.slice(0, 3);
+  const secondRowMembers = supervisoryMembers2526.slice(3);
+
   return (
     <section
       className="flex flex-col gap-20 items-center justify-center w-screen scroll-mt-28 bg-customDarkPurple"
@@ -13,10 +16,19 @@ const SupervisoryPage = () => {
           DENETİM KURULUMUZ
         </h2>
       </div>
-      <div className="flex flex-wrap justify-center gap-6 px-10 sm:gap-8 md:gap-12 lg:gap-20 2xl:max-w-[1800px] justify-self-center">
-        {supervisoryMembers2526.map((member, index) => (
-          <BoardMember key={index} member={member} />
-        ))}
+      
+      <div className="flex flex-col gap-12 w-full items-center">
+        <div className="flex flex-wrap justify-center gap-6 px-10 sm:gap-8 md:gap-12 lg:gap-20">
+          {firstRowMembers.map((member, index) => (
+            <BoardMember key={index} member={member} />
+          ))}
+        </div>
+        
+        <div className="flex flex-wrap justify-center gap-6 px-10 sm:gap-8 md:gap-12 lg:gap-20">
+          {secondRowMembers.map((member, index) => (
+            <BoardMember key={index + 3} member={member} />
+          ))}
+        </div>
       </div>
     </section>
   );
